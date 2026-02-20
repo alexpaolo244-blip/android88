2db80a9c6da8f1514438b92058f426a88bcbbd77
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-20 18:01:26 +0000

    Update MainActivity.java

--- app/src/main/java/com/shofyou/app/MainActivity.java
+++ app/src/main/java/com/shofyou/app/MainActivity.java
@@ -34,20 +34,15 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int nightModeFlags =
-                    getResources().getConfiguration().uiMode
-                            & Configuration.UI_MODE_NIGHT_MASK;
-
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
             if (nightModeFlags != Configuration.UI_MODE_NIGHT_YES) {
-                getWindow().getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             }
         }
 
@@ -59,81 +54,86 @@ public class MainActivity extends AppCompatActivity {
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
         ws.setAllowFileAccess(true);
+        ws.setAllowContentAccess(true);
         ws.setMediaPlaybackRequiresUserGesture(false);
+        ws.setCacheMode(WebSettings.LOAD_DEFAULT);
 
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
 
         webView.setWebViewClient(new WebViewClient() {
-
             @Override
             public void onPageFinished(WebView view, String url) {
                 splashLogo.setVisibility(View.GONE);
                 swipe.setRefreshing(false);
-
                 if (url != null && url.contains("/reels/")) {
                     swipe.setEnabled(false);
                 } else {
                     swipe.setEnabled(true);
                 }
             }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (url.contains("shofyou.com")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                startActivity(new Intent(MainActivity.this, PopupActivity.class).putExtra("url", url));
+                return true;
+            }
         });
 
         webView.setWebChromeClient(new WebChromeClient() {
+            
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress > 80) {
+                    splashLogo.setVisibility(View.GONE);
+                }
+            }
 
             @Override
-            public boolean onShowFileChooser(WebView webView,
-                                             ValueCallback<Uri[]> callback,
-                                             FileChooserParams params) {
-
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> callback, FileChooserParams params) {
+                if (fileCallback != null) {
+                    fileCallback.onReceiveValue(null);
+                }
                 fileCallback = callback;
 
-                boolean isVideo = false;
-                boolean isImage = false;
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                
+                // فحص دقيق جداً لنوع الطلب من الموقع
+                String[] acceptTypes = params.getAcceptTypes();
+                String mimeType = "*/*";
 
-                String[] types = params.getAcceptTypes();
-
-                if (types != null) {
-                    for (String t : types) {
-                        if (t == null) continue;
-                        t = t.toLowerCase();
-
-                        if (t.contains("video")) isVideo = true;
-                        if (t.contains("image")) isImage = true;
+                for (String type : acceptTypes) {
+                    if (type.contains("image")) {
+                        mimeType = "image/*";
+                        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        break; 
+                    } else if (type.contains("video")) {
+                        mimeType = "video/*";
+                        intent.setData(MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                        break;
                     }
                 }
 
-                Intent intent = new Intent(Intent.ACTION_PICK);
-
-                if (isVideo && !isImage) {
-
-                    intent.setDataAndType(
-                            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                            "video/*");
-
-                } else if (isImage && !isVideo) {
-
-                    intent.setDataAndType(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            "image/*");
-
-                } else {
-
-                    // في حالة الموقع لم يحدد النوع (*/ *)
-                    intent.setType("*/*");
-                    intent.putExtra(Intent.EXTRA_MIME_TYPES,
-                            new String[]{"image/*", "video/*"});
+                intent.setType(mimeType);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                
+                try {
+                    startActivityForResult(Intent.createChooser(intent, "Select"), 100);
+                } catch (Exception e) {
+                    fileCallback.onReceiveValue(null);
+                    fileCallback = null;
                 }
-
-                startActivityForResult(intent, 100);
                 return true;
             }
         });
 
         swipe.setOnRefreshListener(() -> {
-
             String current = webView.getUrl();
-
             if (current != null && current.contains("/reels/")) {
                 swipe.setRefreshing(false);
             } else {
@@ -146,40 +146,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
-
-        if (fileCallback == null) return;
-
-        Uri[] result = null;
-
-        if (resultCode == RESULT_OK && data != null) {
-            result = new Uri[]{data.getData()};
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            if (fileCallback == null) return;
+            Uri[] results = null;
+            if (resultCode == RESULT_OK && data != null) {
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount();
+                    results = new Uri[count];
+                    for (int i = 0; i < count; i++) {
+                        results[i] = data.getClipData().getItemAt(i).getUri();
+                    }
+                } else if (data.getData() != null) {
+                    results = new Uri[]{data.getData()};
+                }
+            }
+            fileCallback.onReceiveValue(results);
+            fileCallback = null;
         }
-
-        fileCallback.onReceiveValue(result);
-        fileCallback = null;
     }
 
     private void handleBack() {
-
-        getOnBackPressedDispatcher().addCallback(this,
-                new OnBackPressedCallback(true) {
-
-                    @Override
-                    public void handleOnBackPressed() {
-
-                        if (webView.canGoBack())
-                            webView.goBack();
-                        else
-                            new AlertDialog.Builder(MainActivity.this)
-                                    .setMessage("Exit app?")
-                                    .setPositiveButton("Yes",
-                                            (d, i) -> finish())
-                                    .setNegativeButton("No", null)
-                                    .show();
-                    }
-                });
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack())
+                    webView.goBack();
+                else
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("Exit app?")
+                            .setPositiveButton("Yes", (d, i) -> finish())
+                            .setNegativeButton("No", null)
+                            .show();
+            }
+        });
     }
 }
